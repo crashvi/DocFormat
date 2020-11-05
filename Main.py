bc5be6ef62d526eb600107aa62b4dd4e29aa0c2d
crashvi <crash_vi@mail.ru>
2020-11-05 20:26:19 +0600

    настройки main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,16 @@
 from tkinter import *
-root = tkinter(root)
+root = Tk()
 root.title("Лабораторная работа №4 - git (вариант 1)")
-root.giometry("400x300")
-print("liha beda nachalo")
+root.geometry("400x300")
+root.resizable(width=False, height = False)
+
+def exit(self):
+	root.destroy()
+
+root.bind("<Escape>", exit)	
+
+
+
+
+
 root.mainloop()
