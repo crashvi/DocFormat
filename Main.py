2f2d684c485fc3092e2d8beeef7ba909d9cf599d
crashvi <crash_vi@mail.ru>
2020-11-08 00:19:09 +0600

    about menu changes

--- Main.py
+++ Main.py
@@ -1,19 +1,44 @@
 from tkinter import *
+from PIL import Image, ImageTk
+
+
 root = Tk()
 root.title("Лабораторная работа №4 - git (вариант 1)")
 root.geometry("400x300")
 root.resizable(width=False, height = False)
 
-def exit(self):
+def close_win(self):
 	root.destroy()
 
-root.bind("<Escape>", exit)	
+def new_win():
+	win=Toplevel(root)
+
+def about_win():
+	win=Toplevel(root)
+	win.title("About")
+	win.geometry("582x218")
+	frame=Frame(win)
+	frame.pack()
+	canvas = Canvas(win, height=218, width=582, bg='red')
+	canvas.pack()
+	image = Image.open("0.png")
+	photo = ImageTk.PhotoImage(image)
+	image = canvas.create_image(0, 0, image=phot)
+	but=Button(win)
+	but.pack()
+	
+root.bind("<Escape>", close_win)	
 
 tex=Text(root)
 tex.pack()
 
 m=Menu(root)
 root.config(menu=m)
+
+
+
+
+
 
 m1=Menu(m)
 m.add_cascade(label="File", menu=m1)
@@ -37,6 +62,6 @@ m3.add_command(label="Распечать текущую дату и время")
 
 m4=Menu(m)
 m.add_cascade(label="About", menu=m4)
-m4.add_command(label="О программе")
+m4.add_command(label="О программе", command=about_win)
 
 root.mainloop()
